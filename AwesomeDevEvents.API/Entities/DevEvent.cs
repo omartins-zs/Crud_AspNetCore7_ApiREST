093893522a83fdf1956bcdde1eecacc765024075
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-23 11:02:26 -0300

    Criando Construtor da Classe DevEvent

--- AwesomeDevEvents.API/Entities/DevEvent.cs
+++ AwesomeDevEvents.API/Entities/DevEvent.cs
@@ -2,6 +2,11 @@
 {
     public class DevEvent
     { 
+        public DevEvent()
+        {
+            Speakers = new List<DevEventSpeaker>();
+            IsDeleted = false;
+        }
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
@@ -9,6 +14,5 @@
         public DateTime EndDate { get; set; }
         public List<DevEventSpeaker> Speakers { get; set; }
         public bool IsDeleted { get; set; }
-
     }
 }
