4c2f0d336ae2488212ad84a4db5687ac5ba2a0bc
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-23 11:18:26 -0300

    Create Contrutor e Lista Automatica ao Instanciar

--- AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
+++ AwesomeDevEvents.API/Persistence/DevEventsDbContext.cs
@@ -5,6 +5,9 @@ namespace AwesomeDevEvents.API.Persistence
     public class DevEventsDbContext
     {
         public List<DevEvent> DevEvents { get; set; }
-        
+        public DevEventsDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
     }
 }
