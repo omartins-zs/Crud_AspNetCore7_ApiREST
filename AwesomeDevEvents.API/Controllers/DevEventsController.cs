f54b186996dd2579330af05b7de55896fca720dd
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-23 10:53:54 -0300

    Criando API Controller DevEventsController

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,14 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+
+    }
+}
