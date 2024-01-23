55e1574746a42eb14cef09072720136c29680bb6
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-23 11:25:40 -0300

    Add Serviço DevEventsDbContext no Program

--- AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents.API/Program.cs
@@ -3,8 +3,9 @@ using AwesomeDevEvents.API.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllers();
+builder.Services.AddSingleton<DevEventsDbContext>();
 
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
