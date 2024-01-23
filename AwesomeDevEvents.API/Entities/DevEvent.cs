53b9c8bb755fd81e4cff31bd261c048e6f95c4d1
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-23 11:00:20 -0300

    Criando Classe DevEvent e Propiedades do DevEvent

---
+++ AwesomeDevEvents.API/Entities/DevEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace AwesomeDevEvents.API.Entities
+{
+    public class DevEvent
+    { 
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DevEventSpeaker> Speakers { get; set; }
+        public bool IsDeleted { get; set; }
+
+    }
+}
