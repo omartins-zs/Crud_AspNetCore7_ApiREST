3d833b2145a4856a8a83caa413d2bb433d10b581
omartins-zs <gabrielmatheus209@gmail.com>
2024-01-23 11:09:30 -0300

    Criando propiedades do DevEventSpeaker

--- AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
+++ AwesomeDevEvents.API/Entities/DevEventSpeaker.cs
@@ -2,6 +2,14 @@
 {
     public class DevEventSpeaker
     {
-       
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string TalkTitle { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedInProfile { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<DevEventSpeaker> Speakers { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
