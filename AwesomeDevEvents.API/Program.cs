9d04ae7beaeae0e01dfd629f6c9b3c6b989e17c4
omartins-zs <gabrielmatheus209@gmail.com>
2024-02-07 10:24:34 -0300

    Extend e Configurando Swagger para Documentação

--- AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents.API/Program.cs
@@ -1,5 +1,6 @@
 using AwesomeDevEvents.API.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,24 @@ builder.Services.AddDbContext<DevEventsDbContext>(o => o.UseSqlServer(connection
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "AwesomeDevEvents.API",
+        Version = "v1",
+        Contact = new OpenApiContact
+        {
+            Name = "Test",
+            Email = "teste@email.com",
+            Url = new Uri("youtube.com")
+        }
+    });
+
+    var xmlFile = "AwesomeDevEvents.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
