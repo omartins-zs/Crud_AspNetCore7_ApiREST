5a87ff88bb39e97c5917e57e1d9e8bfed52389d2
omartins-zs <gabrielmatheus209@gmail.com>
2024-02-07 14:52:59 -0300

    Instanciando AutoMapper no Program

--- AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents.API/Program.cs
@@ -1,3 +1,4 @@
+using AwesomeDevEvents.API.Mappers;
 using AwesomeDevEvents.API.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
@@ -7,7 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DevEventsCs");
 
+// builder.Services.AddDbContext<DevEventsDbContext>(o => o.UseInMemoryDatabase("DevEventsDb"));
+
 builder.Services.AddDbContext<DevEventsDbContext>(o => o.UseSqlServer(connectionString));
+
+builder.Services.AddAutoMapper(typeof(DevEventProfile).Assembly);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
